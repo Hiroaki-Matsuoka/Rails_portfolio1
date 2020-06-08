e0fd955b4c7dd72ad40a730aade5e7e4e7a785d4
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-06-08 13:48:10 +0900

    add_user_index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :users, only: :show
+  resources :users, only: [:index, :show]
   root to: 'items#index'
   resources :items
 end
