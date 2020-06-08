c70f668adc843131fd705a40b17af219be521cb8
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-06-08 18:23:10 +0900

    add_place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,54 @@
 class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.create(place_params)
+    if params[:place][:image]
+      @place.image_name = "#{@place.id}.jpg"
+      @place.save
+      binding.pry
+      image = params[:place][:image]
+      File.binwrite("public/place_images/#{@place.image_name}", image.read)
+    end
+  end
+
+  def edit
+    @place = Place.find_by(id: params[:id])
+  end
+
+  def update
+    @place = Place.find_by(id: params[:id])
+    @place.name = params[:name]
+    @place.message = params[:message]
+    @place.image = params[:image]
+    @place.update(place_params)
+    if params[:place][:image]
+      @place.image_name = "#{@place.id}.jpg"
+      @place.save
+      image = params[:place][:image]
+      File.binwrite("public/place_images/#{@place.image_name}", image.read)
+    end
+  end
+
+  def show
+    @place = Place.find_by(id: params[:id])
+  end
+
+  def destroy
+    place = Place.find(params[:id])
+    place.destroy
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :message, :image_name, :image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   root to: 'items#index'
   resources :items
+  resources :places
 end
