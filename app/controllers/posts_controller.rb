86fe5c5b8013830deb28be6a85b231b0dffa8e03
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-06-12 13:14:00 +0900

    scss調整

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(
+      title: post_params[:title],
+      message: post_params[:message],
+      user_id: current_user.id
+    )
+    # redirect_to("posts_path")
+  end
+
+private
+  def post_params
+    params.permit(:title, :message, :user_id)
+  end
+end
