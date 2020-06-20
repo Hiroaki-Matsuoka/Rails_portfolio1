b0867699fc93ec24544e599bceef8f06cd1e3b76
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-06-20 21:27:16 +0900

    add_rspec

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'pry-byebug'
+  gem 'rspec-rails'
 end
 
 group :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
   has_many :items
   has_many :places
   has_many :posts
+
+  # attr_accessor :name, :age
+  #
+  # def initialize(name:, age:)
+  #   @name = name
+  #   @age = age
+  # end
+  #
+  # def disp_name
+  #   if @age > 19
+  #     "#{@name}さん"
+  #   elsif @age > 10
+  #     "#{@name}君"
+  #   elsif @age > 0
+  #     "#{@name}ちゃん"
+  #   else
+  #     "不正な値です"
+  #   end
+  # end
+
 end
