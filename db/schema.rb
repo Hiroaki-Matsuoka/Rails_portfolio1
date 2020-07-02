d2148412e4355bfad48984e021cfeaf72d7185f1
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-07-02 16:30:44 +0900

    items_imageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_120317) do
+ActiveRecord::Schema.define(version: 2020_07_02_071405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_120317) do
     t.datetime "updated_at", null: false
     t.string "image_name"
     t.text "message"
+    t.string "image"
   end
 
   create_table "likes", force: :cascade do |t|
